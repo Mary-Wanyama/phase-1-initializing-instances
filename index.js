1c17ab35099dda2098ff9997bb0d2d69bcf09aca
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-13 05:48:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+
+
+ class breakFast {
+    constructor(food, drink){
+        this.food = food
+        this.drink = drink
+    }
+ }
+
+ class lunch {
+    constructor(food, soup, salad){
+        this.food = food
+        this.soup = soup
+        this.salad= salad
+    }
+ }
+
+ class dinner {
+    #dessert
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this.#dessert = dessert
+    }
+ }
